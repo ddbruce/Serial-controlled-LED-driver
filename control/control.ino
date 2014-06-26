e36332e14614741a3892b1e8545c3020b2e3b3fa
ddbruce <daniel@danieldbruce.com>
2014-06-25 23:50:30 -0400

    *add shiba talk here* look at the diff

--- control/control.ino
+++ control/control.ino
@@ -16,9 +16,38 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+ 
+ /*
+  *
+  * Note: You can tell the difference between Dan and Ethan's code because Dan rarely comments.
+  * If you see a bunch of code without comments, it's most likely Dan's.
+  *
+  */
+
+#include <Wire.h>
+ 
+volatile int mode = 0; // current mode
+int button = 2; // pin of button
+volatile bool bP = false; // button pressed flag to exit current modes
+ 
+int potValue = 0; // value of pot
+volatile bool sevSegOff = true;
+
+// values used for mode set/buttton debouncing
+volatile long modeSetTime = 0;
+  
+// definition of registers on the pwm chip
+#define LED0_ON_L 0x6
+#define LED0_ON_H 0x7
+#define LED0_OFF_L 0x8
+#define LED0_OFF_H 0x9
+
+#define PWM_ADDR 0x40 //address of the PWM chip
 
 void setup(){
   Serial.begin(9600);
+  Wire.begin();
+  setConfiguration();
 }
 
 void loop(){
@@ -37,6 +66,7 @@ void parse(String hexch) {
   g = (15 * hex2int(hexch.substring(2,3))) + hex2int(hexch.substring(3,4));
   b = (15 * hex2int(hexch.substring(4,5))) + hex2int(hexch.substring(5,6));
   ch = (int)hexch.substring(6,7);
+  change(r,g,b,ch);
 }
 
 int hex2int(char x) {
@@ -56,4 +86,43 @@ int hex2int(char x) {
     default:
       return (int)x;
   }
+}
+
+void change(int r, int g, int b, int ch) {
+  setPWM(ch*3,r/255*4095);
+  setPWM(ch*3+1,g/255*4095);
+  setPWM(ch*3+2,b/255*4095);
+}
+
+/*
+ * setPWM brightness on channel given over I2C
+ */
+void setPWM(int channel, uint16_t brightness)
+{
+  Wire.beginTransmission(PWM_ADDR);
+  Wire.write(LED0_ON_L+4*channel);
+  Wire.write(0x00); //turn the LED on at 0
+  Wire.write(0x00); //turn the LED on at 0
+  
+  //turn the LED off when it hits this value (out of 4095)
+  Wire.write(brightness); //first four LSB
+  Wire.write(brightness>>8); //last four MSB
+  Wire.endTransmission();
+}
+
+
+/*
+ * Configure the PWM chip for easy suage and external MOSFET drive
+ */
+void setConfiguration()
+{
+  Wire.beginTransmission(PWM_ADDR);
+  Wire.write(0x00); //enter Mode 1 Register
+  Wire.write(0xa1); //enable ocsillator and auto-increment register and restart
+  Wire.endTransmission();
+  delayMicroseconds(500);//500us delay required after reset
+  Wire.beginTransmission(PWM_ADDR);
+  Wire.write(0x01); //enter Mode 2 Register
+  Wire.write(0x04); //set drive mode for external MOSFETS 
+  Wire.endTransmission();
 }
