140cb64aa15d9ae5f805e0ab97b58cc75dff21b8
ddbruce <daniel@danieldbruce.com>
2014-06-28 00:44:35 -0400

    fixed README command and change script

--- web/change.php
+++ web/change.php
@@ -1,7 +1,7 @@
 <?php 
 $channel = $_POST["channel"];
 $color = $_POST["color"];
-$com = "echo ".$color.$channel."> /dev/arduino";
+$com = "echo ".'"'.$color.$channel.'"'."> /dev/arduino";
 //echo "$channel: $color"
 $output1 = shell_exec($com)
 ?>
