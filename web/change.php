eec435a7cca90d955083d799f730f64e0ec8490e
ddbruce <daniel@danieldbruce.com>
2014-06-29 16:39:51 -0400

    added json save support

--- web/change.php
+++ web/change.php
@@ -1,7 +1,9 @@
 <?php 
 $channel = $_POST["channel"];
 $color = $_POST["color"];
+$settings = $_POST["settings"];
 $com = "echo ".'"'.$color.$channel.'"'."> /dev/arduino";
-//echo "$channel: $color"
-$output1 = shell_exec($com)
+	//echo "$channel: $color"
+$output1 = shell_exec($com);
+file_put_contents("data/settings.js", $settings);
 ?>
---
+++ web/js/run.js
@@ -0,0 +1,121 @@
+$(document).bind('touchmove', function(e) {
+	e.preventDefault();
+});
+function colorbg(id){
+	var color = $(id).chromoselector('getColor');
+	$(id).css({
+		'background-color': color.getHexString(),
+		'color': color.getHexString()
+	});
+}	
+function postphp(id,channel) {
+	$.getJSON('data/settings.js', function(settings) {
+		var color = $(id).chromoselector('getColor').getHexString().substring(1);
+		settings.setting[0].channels[channel-1].hex = color;
+		settingsjson = JSON.stringify(settings);
+		//console.log(ch1 + "," + ch2 + "," + ch3 + "," + ch4 + "," + ch5);
+		$.ajax({
+			url : "change.php",
+			type: "POST",
+			data: {
+				channel: channel,
+				color: color,
+				settings: settingsjson,
+			},
+			success: function(data) {
+				$("#simple-msg").html('<pre><code>' + data +  '</code></pre>');
+			}
+		});
+	});
+}
+$(document).ready(function() {
+	$.getJSON('data/settings.js', function(settings) {
+		var current = settings.setting[0].channels
+		$.fn.chromoselector
+		.defaults("width", 180)
+		.defaults("ringwidth", 50)
+		.defaults("resizable", false)
+		.defaults("speed", 250)
+
+		$("#ch1").chromoselector({
+			create: function() {
+				colorbg(this);
+				$(this).chromoselector('setColor',"#" + current[0].hex);
+			},
+			update: function() {
+				colorbg(this);
+				postphp(this,1);
+			},
+			str2color: function (str) {
+				return '#' + str;
+			},
+			color2str: function (color) {
+				return color.getHexString().substring(1);
+			}
+		});
+		$("#ch2").chromoselector({
+			create: function() {
+				colorbg(this);
+				$(this).chromoselector('setColor',"#" + current[1].hex);
+			},
+			update: function() {
+				colorbg(this);
+				postphp(this,2);
+			},
+			str2color: function (str) {
+				return '#' + str;
+			},
+			color2str: function (color) {
+				return color.getHexString().substring(1);
+			}
+		});
+		$("#ch3").chromoselector({
+			create: function() {
+				colorbg(this);
+				$(this).chromoselector('setColor',"#" + current[2].hex);
+			},
+			update: function() {
+				colorbg(this);
+				postphp(this,3);
+			},
+			str2color: function (str) {
+				return '#' + str;
+			},
+			color2str: function (color) {
+				return color.getHexString().substring(1);
+			}
+		});
+		$("#ch4").chromoselector({
+			create: function() {
+				colorbg(this);
+				$(this).chromoselector('setColor',"#" + current[3].hex);
+			},
+			update: function() {
+				colorbg(this);
+				postphp(this,4);
+			},
+			str2color: function (str) {
+				return '#' + str;
+			},
+			color2str: function (color) {
+				return color.getHexString().substring(1);
+			}
+		});
+		$("#ch5").chromoselector({
+			create: function() {
+				colorbg(this);
+				$(this).chromoselector('setColor',"#" + current[4].hex);
+			},
+			update: function() {
+				colorbg(this);
+				postphp(this,5);
+			},
+			str2color: function (str) {
+				return '#' + str;
+			},
+			color2str: function (color) {
+				return color.getHexString().substring(1);
+			}
+		});
+	});
+});
